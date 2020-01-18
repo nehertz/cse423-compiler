3ee4a32b719a6ce5f00a336ef507b8a863687614
AnhaoXiang <anhaoxiang@cs.nmt.edu>
2020-01-18 16:32:33 -0700

    Included all keywords to test2.c

--- test-cases/test2.c
+++ test-cases/test2.c
@@ -4,15 +4,17 @@ int main()
         auto b = 99;
         extern double c = 32.2;
         struct d;
+        typedef signed long slong;
         char e = 'e';
         union f;
         register short g = 1;
         long h = 99999999;
         static volatile enum i;
         const float j = 3.14;
+        int arr[10];
 
         if (a + b - c * g / j % h == 42) {
-
+                
         } else if (a != b && a > b || !(a < b)) {
                 if (a <= b) {
                         return 1;
@@ -24,10 +26,33 @@ int main()
                 return 3;
         }
 
+        for(a = 0; a < 10; a++){
+                if(a < 8){
+                        continue;
+                }        
+        }
+
+        switch (a)
+        {
+                case 9: 
+                        a = 1;
+                        break;
+        
+                default:
+                        a = 0; 
+                        break;
+        }
+
+
+
         label:
                 printf("label");
 
-        
+        do {
+                a = a + 1;
+
+        }while( a < 10);
+
 
         return 0;
 }
@@ -37,6 +62,7 @@ void func(int param1, int param2) {
         int bb = 2;
         int cc = 3;
 
+        //test assignment operators
         aa += 1;
         bb -= 2;
         cc *= 9;
@@ -48,11 +74,14 @@ void func(int param1, int param2) {
         bb ^= 1;
         bb |= 1;
 
+        //test In/Decrement operators
         cc++;
         cc--;
 
+        //test Conditional operators
         int check == aa == 3 ? aa > 3 : return;
 
+        //test Bitwise operators
         dd = cc >> 3;
         ee = dd << 1;
         ff = ~ee;
@@ -60,10 +89,23 @@ void func(int param1, int param2) {
         hh = gg | 0;
         ii = gg ^ 1;
 
+        //test Special operators
         int size = sizeof(aa);
-
         char *apple = "apple!";
         int where = &apple;
 
         return;
+}
+
+void func2(int param1) {
+        if(param1 % 2 == 0){
+                goto even;
+        } else {
+                goto odd;
+        }
+
+        even: 
+                return;
+        odd:
+                return;
 }
