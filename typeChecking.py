64fd6afa0dc10b678c1e5abfafd62b22edb8de72
Yash <yashshah.family@gmail.com>
2020-03-02 08:14:58 -0700

    type-checking mostly donee

--- typeChecking.py
+++ typeChecking.py
@@ -3,6 +3,7 @@ from io import BytesIO
 from io import StringIO
 from skbio import read
 from skbio.tree import TreeNode
+import re
 # from ply_parser import st
 
 class typeChecking:
@@ -11,14 +12,39 @@ class typeChecking:
         def checkTypes(self, treeString, st):
                 tree = TreeNode.read(StringIO(treeString))
                 count = 0
+                assignOps = re.compile("(=)|(\+=)|(-=)|(\*=)|(/=)|(%=)|(<<=)|(>>=)|(&=)|(\^=)|(\|=)")
+                operations = re.compile("(\|\|)|(&&)|(<<)|(>>)|(&)|(\|)|(\^)|(\/)|(\+)|(\-)|(\*)|(\%)|(==)|(!=)|(>=)|(<=)")
                 for node in tree.preorder():
-                        if (node.name == '='):
+                        if (assignOps.match(node.name)):
                                 continue
                         else: 
                                 count +=1
                                 if (count == 1):
                                         type1 = st.lookup(node.name)
                                 elif(count >= 2):
-                                        if ('+-*/'.find(node.name) != -1):
-                                                
-                                        
+                                        # if ('+-*/%&|^'.find(node.name) != -1):
+                                        if (operations.match(node.name)):
+                                                continue
+                                        else:
+                                                if (self.getTypeSimilarity(type1, node.name, st)):
+                                                        # print("good")
+                                                        continue
+                                                else:
+                                                        # print ("not good")
+                                                        print("types not matched. Type conversion required which is not supproted")
+                                                        continue
+
+        
+        def getTypeSimilarity(self, type1, token, st):
+                number = re.compile('\d+.{0,1}\d*')
+                if (number.match(token)):
+                        print("it's a number  " + token)
+                        return False 
+                else:
+                        type2 = st.lookup(token)
+                        if (type1 == type2):
+                                print('types matched   ' + token)
+                                return True 
+                        else: 
+                                print('types not matched   ' + token)
+                                return False
