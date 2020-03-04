4c494b79e433fa8be18716443fb4baa465dffdde
Yash <yashshah.family@gmail.com>
2020-03-03 23:32:25 -0700

    Updated. We support multiple lines expr assignment.
    Still WIP

--- IR.py
+++ IR.py
@@ -3,7 +3,7 @@ from io import BytesIO
 from io import StringIO
 from skbio import read
 from skbio.tree import TreeNode
-
+from skbio.tree import MissingNodeError
 class IR:
         def __init__(self, ast):
                 self.IRStructure = []
@@ -14,12 +14,17 @@ class IR:
         def run(self):
                 for node in self.tree.traverse():
                         if (node.name == 'stmt'):
-                                self.statementNode(node)
+                                for child in node.children:
+                                        self.statementNode(child)
+                                        
+                                        # if (node.distance(self.tree.find(name)) <= 2):
+                                                # print(name)
                 
 
         def statementNode(self, node):
                 # equalNode = node.neighbors()
                 equalEncountered = False
+                exprNode = TreeNode()
                 expr = False
                 for node1 in node.traverse():
                         if (node1.name == '='):
@@ -33,12 +38,19 @@ class IR:
                                 exprNode = node1
                                 print('in here')
                                 break
+                # copy_exprNode = exprNode.copy()
+                # notFound = False
+                seenNodes = []
                 for node1 in exprNode.tips():
+                        if (node1 in seenNodes):
+                                continue
                         siblings = node1.siblings()
                         parent = node1.ancestors()[0]
+                        # exprNode.remove(siblings[0])
+                        seenNodes.append(siblings[0])
+                        
                         s = '_' + str(self.variables)
                         self.variables += 1
-
                         if ('+-/*'.find(parent.name) != -1):
                                 s = s + ' = ' + node1.name + ' ' + parent.name + ' ' + siblings[0].name
                         print(s)
---
+++ tree.py
@@ -0,0 +1,11 @@
+class Tree:
+        def __init__(self):
+                self.tree = []
+                self.root = None
+                self.children = None
+        def add_root(self, root):
+                self.tree.append(root)
+                self.root = root
+
+        def traverse(self, )
+        
