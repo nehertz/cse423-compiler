11f50728be06c5b02b1d2eee835c44888427a305
Yash <yashshah.family@gmail.com>
2020-03-03 22:05:11 -0700

    basic code generation happens. redundancy  not eliminated yet

--- IR.py
+++ IR.py
@@ -7,8 +7,9 @@ from skbio.tree import TreeNode
 class IR:
         def __init__(self, ast):
                 self.IRStructure = []
+                ast = ast.replace('\"', '')
                 self.tree = TreeNode.read(StringIO(ast))
-
+                self.variables = 1
 
         def run(self):
                 for node in self.tree.traverse():
@@ -17,8 +18,27 @@ class IR:
                 
 
         def statementNode(self, node):
-                equalNode = node.neighbors()
-                print(equalNode)
-                # if ('=' in equalNode):
-                #         for node1 in equalNode.tips():
-                #                 print(node1.name)
+                # equalNode = node.neighbors()
+                equalEncountered = False
+                expr = False
+                for node1 in node.traverse():
+                        if (node1.name == '='):
+                                equalEncountered = True
+                                continue 
+                        if (equalEncountered):
+                                equalEncountered = False 
+                                expr = True
+                                continue
+                        if (expr):
+                                exprNode = node1
+                                print('in here')
+                                break
+                for node1 in exprNode.tips():
+                        siblings = node1.siblings()
+                        parent = node1.ancestors()[0]
+                        s = '_' + str(self.variables)
+                        self.variables += 1
+
+                        if ('+-/*'.find(parent.name) != -1):
+                                s = s + ' = ' + node1.name + ' ' + parent.name + ' ' + siblings[0].name
+                        print(s)
