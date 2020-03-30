7513193b39571533ca5a43a04c9e111949f4084d
dopamine <yashshah.family@gmail.com>
2020-03-29 19:45:37 -0600

    typeConversion class created for type conversion

---
+++ typeConversion.py
@@ -0,0 +1,242 @@
+class TypeConversion:
+    def convertInt2Float(self, expr):
+        s = expr + '.00'
+        return s
+    def convertInt2Char(self, expr):
+        num = int(expr)
+        num &= 0xFF
+        return str(num)
+    def convertInt2Short(self, expr):
+        num = int(expr)
+        num &= 0xFFFF
+        return str(num)
+    def convertInt2Double(self, expr):
+        return self.convertInt2Float(expr)
+    def convertInt2UInt(self, expr):
+        return 
+    def convertInt2Long(self, expr):
+        return 
+    def convertInt2LongLong(self, expr):
+        return 
+    def convertInt2ULong(self, expr):
+        return 
+    def convertInt2UShort(self, expr):
+        return 
+    def convertInt2UChar(self, expr):
+        return 
+
+
+
+    def convertFloat2Int(self, expr):
+        return 
+    def convertFloat2Char(self, expr):
+        return 
+    def convertFloat2Short(self, expr):
+        return 
+    def convertFloat2Double(self, expr):
+        return 
+    def convertFloat2UInt(self, expr):
+        return 
+    def convertFloat2Long(self, expr):
+        return
+    def convertFloat2LongLong(self, expr):
+        return 
+    def convertFloat2ULong(self, expr):
+        return 
+    def convertFloat2UShort(self, expr):
+        return 
+    def convertFloat2UChar(self, expr):
+        return 
+    
+    
+    
+    
+    def convertChar2Int(self, expr):
+        return 
+    def convertChar2Float(self, expr):
+        return 
+    def convertChar2Short(self, expr):
+        return 
+    def convertChar2Double(self, expr):
+        return 
+    def convertChar2UInt(self, expr):
+        return 
+    def convertChar2Long(self, expr):
+        return
+    def convertChar2LongLong(self, expr):
+        return 
+    def convertChar2ULong(self, expr):
+        return 
+    def convertChar2UShort(self, expr):
+        return 
+    def convertChar2UChar(self, expr):
+        return 
+
+    
+    def convertShort2Int(self, expr):
+        return 
+    def convertShort2Char(self, expr):
+        return 
+    def convertShort2Float(self, expr):
+        return 
+    def convertShort2Double(self, expr):
+        return 
+    def convertShort2UInt(self, expr):
+        return 
+    def convertShort2Long(self, expr):
+        return
+    def convertShort2LongLong(self, expr):
+        return 
+    def convertShort2ULong(self, expr):
+        return 
+    def convertShort2UShort(self, expr):
+        return 
+    def convertShort2UChar(self, expr):
+        return 
+
+    def convertDouble2Int(self, expr):
+        return 
+    def convertDouble2Char(self, expr):
+        return 
+    def convertDouble2Short(self, expr):
+        return 
+    def convertDouble2Float(self, expr):
+        return 
+    def convertDouble2UInt(self, expr):
+        return 
+    def convertDouble2Long(self, expr):
+        return
+    def convertDouble2LongLong(self, expr):
+        return 
+    def convertDouble2ULong(self, expr):
+        return 
+    def convertDouble2UShort(self, expr):
+        return 
+    def convertDouble2UChar(self, expr):
+        return 
+
+    def convertUInt2Int(self, expr):
+        return 
+    def convertUInt2Char(self, expr):
+        return 
+    def convertUInt2Short(self, expr):
+        return 
+    def convertUInt2Double(self, expr):
+        return 
+    def convertUInt2Float(self, expr):
+        return 
+    def convertUInt2Long(self, expr):
+        return
+    def convertUInt2LongLong(self, expr):
+        return 
+    def convertUInt2ULong(self, expr):
+        return 
+    def convertUInt2UShort(self, expr):
+        return 
+    def convertUInt2UChar(self, expr):
+        return 
+
+    def convertLong2Int(self, expr):
+        return 
+    def convertLong2Char(self, expr):
+        return 
+    def convertLong2Short(self, expr):
+        return 
+    def convertLong2Double(self, expr):
+        return 
+    def convertLong2UInt(self, expr):
+        return 
+    def convertLong2Float(self, expr):
+        return
+    def convertLong2LongLong(self, expr):
+        return 
+    def convertLong2ULong(self, expr):
+        return 
+    def convertLong2UShort(self, expr):
+        return 
+    def convertLong2UChar(self, expr):
+        return
+
+    def convertLongLong2Int(self, expr):
+        return 
+    def convertLongLong2Char(self, expr):
+        return 
+    def convertLongLong2Short(self, expr):
+        return 
+    def convertLongLong2Double(self, expr):
+        return 
+    def convertLongLong2UInt(self, expr):
+        return 
+    def convertLongLong2Long(self, expr):
+        return
+    def convertLongLong2Float(self, expr):
+        return 
+    def convertLongLong2ULong(self, expr):
+        return 
+    def convertLongLong2UShort(self, expr):
+        return 
+    def convertLongLong2UChar(self, expr):
+        return  
+    
+    def convertUShort2Int(self, expr):
+        return 
+    def convertUShort2Char(self, expr):
+        return 
+    def convertUShort2Short(self, expr):
+        return 
+    def convertUShort2Double(self, expr):
+        return 
+    def convertUShort2UInt(self, expr):
+        return 
+    def convertUShort2Long(self, expr):
+        return
+    def convertUShort2LongLong(self, expr):
+        return 
+    def convertUShort2ULong(self, expr):
+        return 
+    def convertUShort2Float(self, expr):
+        return 
+    def convertUShort2UChar(self, expr):
+        return 
+
+    def convertULong2Int(self, expr):
+        return 
+    def convertULong2Char(self, expr):
+        return 
+    def convertULong2Short(self, expr):
+        return 
+    def convertULong2Double(self, expr):
+        return 
+    def convertULong2UInt(self, expr):
+        return 
+    def convertULong2Long(self, expr):
+        return
+    def convertULong2LongLong(self, expr):
+        return 
+    def convertULong2Float(self, expr):
+        return 
+    def convertULong2UShort(self, expr):
+        return 
+    def convertULong2UChar(self, expr):
+        return 
+
+    def convertUChar2Int(self, expr):
+        return 
+    def convertUChar2Char(self, expr):
+        return 
+    def convertUChar2Short(self, expr):
+        return 
+    def convertUChar2Double(self, expr):
+        return 
+    def convertUChar2UInt(self, expr):
+        return 
+    def convertUChar2Long(self, expr):
+        return
+    def convertUChar2LongLong(self, expr):
+        return 
+    def convertUChar2ULong(self, expr):
+        return 
+    def convertUChar2UShort(self, expr):
+        return 
+    def convertUChar2Float(self, expr):
+        return 
