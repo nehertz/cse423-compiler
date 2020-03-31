a091590c0f9a28242c1ff77a0f1ee3d8debef65c
AnhaoXiang <xianganhao@gmail.com>
2020-03-31 16:19:12 -0600

    added options required in project 3, added basic support for optimization pass

---
+++ optimization.py
@@ -0,0 +1,10 @@
+class optimization:
+    def __init__(self, IR):
+        self.IRS = IR
+        self.optimizedIR = [] 
+    
+    def run(self):
+        return self.IRS
+    
+    def printIR(self):
+        pass
