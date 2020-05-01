fb3dea9fd74e49ce8896243afee603f5466146c1
dopamine <yashshah.family@gmail.com>
2020-04-30 18:17:04 -0600

    assignment updated
    Now we support assignment. Pure assignment is type of  lvalue = rvalue. So, rvalue will be assigned a register, but lvalue should not need to. As you can use move from register to memory.

--- symbolTableRegisters.py
+++ symbolTableRegisters.py
@@ -14,12 +14,14 @@ class SymbolTableRegisters:
     def movFromReg2Mem(self, reg):
         # updates the symbol table
         # returns the assembly code
+        assCode = ''
         tmpVar = self.symboltable_reg[reg]
         if (self.number.match(tmpVar)):
             self.symboltable_reg[reg] = ''
+            # assCode += 'push ' + str(reg) 
         else:
             memAddr = self.symboltable_mem[tmpVar]
-            assCode = 'mov ' + str(self.symboltable_reg[reg]) + ' ' + str(memAddr) + '\n'
+            assCode += 'mov ' + str(self.symboltable_reg[reg]) + ' ' + str(memAddr) + '\n'
             self.symboltable_reg[reg] = ''
         return assCode
     # 4(%rsp)
@@ -73,13 +75,46 @@ class SymbolTableRegisters:
         '''
         1. Caller-saved Register: %eax, %ecx, %edx. 
         caller should push these values on the stack. 
-        2. Pass the parameters by pushing them on stack and also last parameter first order (inverted).
-        3. push the return address on top of the stack. 
+        2. First push these registers, then Pass the parameters by pushing them on stack and also last parameter first order (inverted).
+
         '''
-            
-
-
+        assCode = ''
+        assCode += 'push ' + '%rax' + '\n'
+        assCode += 'push ' + '%rcx' + '\n'
+        assCode += 'push ' + '%rdx' + '\n'
         return assCode
+
+    def calleeSavedReg(self):
+        '''
+        1. Call this after all the parameters have been pushed.
+        '''
+        assCode = ''
+        assCode += 'push ' + '%rbx' + '\n'
+        assCode += 'push ' + '%rdi' + '\n'
+        assCode += 'push ' + '%rsi' + '\n'
+        return assCode
+    def calleeEnd(self):
+        '''
+        1. Return Value is assumed to be in %eax 
+        2. Stack has this contents: local parameters, %rbx, %rdi, %rsi
+        3. This function performs popping of the stack values to the callee saved registers: %rbx, %rdi, %rsi
+        '''
+        assCode = ''
+        assCode += 'pop ' + '%rsi' + '\n'
+        assCode += 'pop ' + '%rdi' + '\n'
+        assCode += 'pop ' + '%rsi' + '\n'
+        return assCode
+    def afterFunctionCall(self):
+        '''
+        1. Call this after return value has been correctly moved from %eax. 
+        2. pop stack members in %rdx, %rcx, and %rax - in that order
+        '''
+        assCode = ''
+        assCode += 'pop ' + '%rdx' + '\n'
+        assCode += 'pop ' + '%rcx' + '\n'
+        assCode += 'pop ' + '%rax' + '\n'
+        return assCode
+
     def check_reg_status(self, reg):
         if (self.symboltable_reg[reg] == ''):
             return True
